8d14eebaa30ef94c459e8f2b343574d3dc92470a
Jinglin Xu <jixu@ansys.com>
2016-09-01 14:21:01 +0800

    Packing the function with argument of block ID, get image by it's name

--- insertPicture.js
+++ insertPicture.js
@@ -1,13 +1,11 @@
-	
-	    oListbox = document.getElementById("Temperature");  
 
-		function insertPicture(){
-		    var value = oListbox.select;			
-		    var indx = oListbox.selectedIndex;
-			//alert(oListbox.options[indx].firstChild.nodeValue); //显示的内容 'Second_Layer'   
-            //alert(oListbox.options[indx].getAttribute("value"));//对应的value  '2'			
-			
-			path = "Image/" + (indx + 1) + ".jpg";		
-            var divImage = document.getElementById('ChipLayerTemperature');
+
+		function insertPicture(elmtID){		
+	        oListbox = document.getElementById(elmtID);  		
+		    var indx = oListbox.selectedIndex;				
+			var value = oListbox.options[indx].firstChild.nodeValue;			
+			path = "Image/" + value + ".jpg";	
+            elmtID += "Image";
+            var divImage = document.getElementById(elmtID);
             divImage.innerHTML = '<img src="'+path+'" />';
 		}
